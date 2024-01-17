38c0cd67c7ecb60d5ae98e1dd84f2d6718c01002
dianacgonzalez <dianaceciliagonzalez@gmail.com>
2024-01-17 18:07:18 -0300

    ADD Archivos3

--- project/config/views.py
+++ project/config/views.py
@@ -2,7 +2,3 @@ from django.http import HttpResponse
 
 def saludo(request):
     return HttpResponse("Hola")
-
-def saludo2(request):
-    nombre = input ("Ingrese su nombre")
-    return HttpResponse(f"Hola {nombre}")
